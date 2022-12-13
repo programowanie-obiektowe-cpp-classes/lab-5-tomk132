e14a6b34583b3161333aa0245055225eec43ca13
kucin <kucin@LAPTOP-2VJU9K9D>
2022-12-13 22:13:54 +0100

    pierwsza wersja zadania zejściowego

--- foo.hpp
+++ foo.hpp
@@ -4,9 +4,31 @@
 
 #include <list>
 #include <vector>
+#include <iostream>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    
+
+    std::vector< char > wynik; 
+
+    std::list< Human >::reverse_iterator it = people.rbegin(); 
+
+    for (auto it = people.rbegin(); it != people.rend(); it++) {
+        it->birthday();
+
+       //std::cout << it->getName() << std::endl; 
+
+        if (it->isMonster()==1) {
+            wynik.emplace_back('n'); 
+           // std::cout << it->isMonster << std::endl;
+        }
+        else
+        {
+            wynik.emplace_back('y');
+        }
+
+    }
+
+    return wynik;
 }
